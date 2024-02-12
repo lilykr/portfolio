a124647b9ec53193092755e171c91b491765912f
lilykr <lisa-lou.k@hotmail.fr>
2024-02-12 17:38:56 +0100

    ui

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -12,7 +12,7 @@ export default function About() {
   const t = useTranslations();
   return (
     <div
-      className="relative z-10 h-[calc(100dvh-23px)] flex flex-col tablet:flex-row tablet:justify-center tablet:items-center tablet:pt-0 pt-[calc((100dvh)/3-100px)] tablet:px-32"
+      className="relative z-10 h-[calc(100dvh-23px)] flex flex-col tablet:flex-row justify-center items-center tablet:px-32 tablet:ml-0 ml-[30px] pt-7"
       id="about-section">
       <div className="flex flex-col items-center tablet:items-start">
         <ParagraphTitle title={t('about_me')} className="max-w-sm" />
@@ -20,9 +20,7 @@ export default function About() {
           <BodyText text="“ Développeuse React fullstack freelance avec trois ans d'expérience, je me distingue par une approche agile de la programmation. Mon engagement ? Collaborer étroitement avec mes clients pour créer des produits sur mesure, alliant la puissance du développement à une interface React élégante. Si vous cherchez une professionnelle du code, passionnée et créative, prête à relever de nouveaux défis, explorons ensemble comment je peux contribuer à votre projet. Découvrez mes expériences professionnelles et projets personnels dans ce portfolio. “" />
         </div>
       </div>
-      <div className="flex items-center w-[50%] m-auto tablet:mt-[0px] tablet:w-[350px] h-full">
-        <Image alt="picture of me" src={me} />
-      </div>
+      <Image alt="picture of me" src={me} className="w-[50%] m-auto tablet:w-80" />
     </div>
   );
 }
--- src/components/atoms/BodyText.tsx
+++ src/components/atoms/BodyText.tsx
@@ -1,7 +1,5 @@
 type Props = { text: string };
 
 export const BodyText = ({ text }: Props) => {
-  return (
-    <p className="text-body11 tablet:text-body13 tablet:text-body16 tablet:text-body20">{text}</p>
-  );
+  return <p className="text-body13 tablet:text-body16 laptop:text-body20">{text}</p>;
 };
